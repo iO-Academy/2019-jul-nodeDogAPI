f39ced7a27a29225e7377787145a2c2775512d5f
Cat <blackcatstone@gmail.com>
2019-10-15 11:41:59 +0100

    Change indentation

--- index.js
+++ index.js
@@ -14,30 +14,30 @@ const dbName = 'hot-dog';
 const collectionName = 'dogs';
 
 app.get('/dogs', (req, res) => {
-  mongoClient.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-    if (err) {
-      return res.status(400).json({
-        success: false,
-        message: 'Error connecting to database.',
-        data: []
-      })
-    }
-    let db = client.db(dbName);
-    let collection = db.collection(collectionName);
-    collection.find({}).toArray((err, docs) => {
-      if (err) {
-        return res.status(400).json({
-          success: true,
-          message: 'Error getting data from database.',
-          data: []
+    mongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                message: 'Error connecting to database.',
+                data: []
+            })
+        }
+        let db = client.db(dbName);
+        let collection = db.collection(collectionName);
+        collection.find({}).toArray((err, docs) => {
+            if (err) {
+                return res.status(400).json({
+                    success: true,
+                    message: 'Error getting data from database.',
+                    data: []
+                });
+            }
+            res.status(200).json({
+                success: true,
+                message: '',
+                data: docs
+            });
+            client.close();
         });
-      } 
-      res.status(200).json({
-        success: true,
-        message: '',
-        data: docs
-      });
-      client.close();
     });
-  });
 });
